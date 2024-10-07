9cf52d7a05b4a0c1adc564c07099749882f9fc13
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2024-10-07 23:09:20 +0200

    navbar

--- src/components/Root/Navbar.jsx
+++ src/components/Root/Navbar.jsx
@@ -4,29 +4,35 @@ import logo from '../../assets/logo.jpeg'
 
 export default function Navbar() {
   return (
-    <nav className='navbar navbar-expand-md bg-body-tertiary' id='navbar'>
-        <div className="container-fluid barre d-flex align-items-center">
-            <NavLink to="" className="navbar-brand d-flex align-items-center">
-                <img src={logo} className='d-inline-block' alt="Logo" width="50" height="50"/>
-            </NavLink>
+    <nav className='navbar navbar-expand' id='navbar'>
+        <div className="container-fluid barre d-flex justify-content-center">
+            <ul className="navbar-nav d-flex align-items-center justify-content-center">
+                <li className="nav-item">
+                    <NavLink to="" className='nav-link'>ACCUEIL</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="sorties" className='nav-link'>INFOS</NavLink>
+                </li>
 
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+                <NavLink to="" className="navbar-brand d-flex align-items-center" id="logo">
+                    <img src={logo} className='d-inline-block' alt="Logo" width="150" height="150"/>
+                </NavLink>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mb-2">
-                    <li className="nav-item">
-                        <NavLink to="" className='nav-link'>Home</NavLink>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        ANNÉES
+                    </a>
+                    <ul class="dropdown-menu">
+                        {[...Array(8)].map((_, i) =>
+                            <li><a class="dropdown-item" href="#">{ 2023 - i }</a></li>
+                        )}
+                    </ul>
                     </li>
-                    <li className="nav-item">
-                        <NavLink to="sorties" className='nav-link'>Sorties</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="shop" className='nav-link'>Goodies</NavLink>
-                    </li>
-                </ul>
-            </div>
+
+                <li className="nav-item">
+                    <NavLink to="shop" className='nav-link'>CONTACT</NavLink>
+                </li>
+            </ul>
         </div>
     </nav>
   )
