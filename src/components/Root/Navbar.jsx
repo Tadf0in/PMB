c8c8d275f027b8ef48547153d2e13de910107324
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2024-10-26 19:21:24 +0200

    galerie

--- src/components/Root/Navbar.jsx
+++ src/components/Root/Navbar.jsx
@@ -13,26 +13,14 @@ export default function Navbar() {
             <li className="nav-item">
                 <NavLink to="/infos" className='nav-link'>INFOS</NavLink>
             </li>
-
             <li className='nav-item' id="logo">
                 <NavLink to="/" onClick={() => window.location.href = "#"}>
                     <img src={logo} className='d-inline-block' alt="Logo"/>
                 </NavLink>
             </li>
-
-            <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    GALERIE
-                </a>
-                <ul className="dropdown-menu" id="nav-dropdown">
-                    {[...Array(20)].map((_, i) =>
-                        <li key={2024-i}>
-                            <NavLink to={ "/galerie/" + (2024 - i).toString() } className='nav-link dropdown-item'>{ 2024 - i }</NavLink>
-                        </li>
-                    )}
-                </ul>
+            <li className="nav-item">
+                <NavLink to="/galerie/2024" className='nav-link'>GALERIE</NavLink>
             </li>
-
             <li className="nav-item">
                 <NavLink to="/" className='nav-link' onClick={() => window.location.href = "#contact"}>CONTACT</NavLink>
             </li>
@@ -54,24 +42,12 @@ export default function Navbar() {
                 <li className="nav-item">
                     <NavLink to="" className='nav-link' onClick={() => window.location.href = "#"}>ACCUEIL</NavLink>
                 </li>
-
                 <li className="nav-item">
                     <NavLink to="infos" className='nav-link'>INFOS</NavLink>
                 </li>
-
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        GALERIE
-                    </a>
-                    <ul className="dropdown-menu" id="nav-dropdown">
-                        {[...Array(20)].map((_, i) =>
-                            <li key={2024-i}>
-                                <NavLink to={ "/galerie/" + (2024 - i).toString() } className='nav-link dropdown-item'>{ 2024 - i }</NavLink>
-                            </li>
-                        )}
-                    </ul>
+                <li className="nav-item">
+                    <NavLink to="/galerie/2024" className='nav-link'>GALERIE</NavLink>
                 </li>
-
                 <li className="nav-item">
                     <NavLink to="" className='nav-link' onClick={() => window.location.href = "#contact"}>CONTACT</NavLink>
                 </li>
