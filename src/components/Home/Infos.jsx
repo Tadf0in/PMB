f62db9bc4f049d95b5f182897bcd7ded26edf534
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2024-11-05 22:19:02 +0100

    texte concret

--- src/components/Home/Infos.jsx
+++ src/components/Home/Infos.jsx
@@ -4,10 +4,17 @@ export default function Infos() {
         <h2 className="rh2">Qui sommes-nous ?</h2>
         <div className='hr'></div>
         <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias sed commodi doloremque? Dolores expedita vitae neque commodi ratione doloremque consequatur laborum, quos fugiat veniam nesciunt excepturi, quas nobis nemo sit.
-            <br />
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, aliquid eveniet reiciendis esse ratione commodi natus aut est tempora nemo aspernatur nesciunt amet facere rerum! Accusamus quasi mollitia labore laudantium?
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi ab aliquam vero ut doloribus repellat, aliquid quaerat enim sunt ipsam perferendis veritatis, sit voluptatum corporis deleniti reiciendis! Modi, nulla magnam.    
+            Polytech Mont Blanc, aussi connue sous le nom de PMB est une association emblématique de Polytech Annecy-Chambéry. 
+            <br/><br/>
+            Depuis de nombreuses années, elle permet de faire découvrir les activités de montagne telles que la randonnée dans les beaux paysages de la Savoie et Haute-Savoie ou bien l’escalade. Malgré notre dépendance au Bureau Des Élèves de Polytech Annecy-Chambéry, nous jouissons d’une liberté de fonctionnement aussi bien au niveau de nos financements que de notre organisation. 
+            <br/><br/>
+            Chaque année, des élèves entrant en 3ème année de cycle ingénieur reprennent l’association en tant que membres du bureau ou membres actifs contribuant à pérenniser une vie associative liée à la montagne au sein de notre université. 
+            <br/><br/>
+            En étant devenue, au fur et à mesure des années, une réelle institution, PMB voit son nombre de membres annuels en constante augmentation, que cela soit sur le campus de Annecy ou de Chambéry.
+            <br/><br/>
+            Offrant la possibilité à tous les étudiants venant d’univers différents de se retrouver autour d’une même passion, PMB crée une réelle synergie au sein même de l'École, malgré deux campus différents. grâce à un but commun et des valeurs communes
+            <br/><br/>
+            Il s’agit d’un projet exceptionnel, où les étudiants partagent des moments forts et des souvenirs qui les accompagneront toute leur vie. Les souvenirs d’une aventure, les souvenirs d’une réussite, des souvenirs qui resteront parmi les plus beaux de leurs études supérieures à l’Université de Savoie.
         </p>
     </section>
     );
--- src/components/Home/Projet.jsx
+++ src/components/Home/Projet.jsx
@@ -9,7 +9,9 @@ export default function Projet() {
             <h2 className="rh2">Le projet</h2>
             <div className="hr"></div>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe dicta libero blanditiis veniam soluta accusamus recusandae deleniti reprehenderit delectus. Ipsam dolor explicabo sint vero quas cumque reprehenderit magnam saepe quo.
+                Depuis 20 ans, les élèves de 3ème année de l’école Polytech Annecy-Chambéry relèvent le défi de gravir le toit de l’Europe.
+                <br/><br/>
+                L’objectif, au-delà de gravir le Mont-Blanc, est avant tout une aventure humaine fondée sur l’esprit d’équipe, la solidarité, la persévérance et le dépassement de soi. Tant de valeurs qui auront toute leur importance dans les futures carrières des étudiants de l’Université de Savoie, et dans leur vie quotidienne.
             </p>
         </div>
     </section>
